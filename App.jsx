494251f687608df0b3e260ce21d1d74b79affe9e
Nicole Hippolite <ncla496@aucklanduni.ac.nz>
2017-09-25 12:17:36 +1300

    uploading initial files

---
+++ App.jsx
@@ -0,0 +1,11 @@
+Import React from 'react';
+    class App extends React.Component {
+        render(){
+            return(
+                <div>
+                hello World!!!
+                </div>
+            );
+        }
+    }
+    export default App;
---
+++ main.js
@@ -0,0 +1,4 @@
+Import React from 'react';
+import ReatDOM from 'react-dom';
+import App from './App.jsx';
+ReactDOM.render(<App/>,document.getElementById('app'));
